7f7f2f5f96c9e21748081a0a34a5681ab914649c
Eric McKay <eric@andcode.co>
2016-05-26 21:06:25 +0000

    find or create by token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find_by(token: params[:token])
+      @user = User.find_or_create_by(token: params[:token])
     end
 
     # Only allow a trusted parameter "white list" through.
