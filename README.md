aeb1a544144a080ff0aa4e647bdd9296be00f205
Eric McKay <eric@andcode.co>
2016-06-05 21:50:37 +0000

    remove token authentication

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # React Blackjack Rails 5 Backend
 
-This is a Rails 5 API for part 4 of the [react blackjack guide](https://eloquent.ly/guides/react-blackjack-part-4).
+This is a Rails 5 (beta) API for part 5 of the [react blackjack guide](https://eloquent.ly/guides/react-blackjack-part-5).
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
 
-You can deploy your own copy to Heroku using the button above.
+You can deploy your own copy to Heroku using the button above. You can enter any string as the API_TOKEN configuration variable.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,3 @@
 class ApplicationController < ActionController::API
     include ActionController::HttpAuthentication::Token::ControllerMethods
-    
-    before_action :authenticate
-
-    protected
-
-    # Authenticate the user with token based authentication
-    def authenticate
-        authenticate_token || render_unauthorized
-    end
-    
-    def authenticate_token
-        authenticate_with_http_token do |token, options|
-            token == ENV['API_TOKEN']
-        end
-    end
-    
-    def render_unauthorized(realm = "Application")
-        self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
-        render json: 'Bad credentials', status: :unauthorized
-    end
 end
